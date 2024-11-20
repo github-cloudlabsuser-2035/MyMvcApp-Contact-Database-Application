9944f8d94ae219b957e8e4ab53924b04e0b0ff82
Tu Nombre <github_cloudlabsuser_2035@cloudlabsaiuser.com>
2024-11-20 17:17:44 +0000

    commit2

--- Program.cs
+++ Program.cs
@@ -10,3 +10,21 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run("http://localhost:5266");
